221100c43ea429217bdeb8a6a5a1368e22b07499
Rana <dorzedumzan@gmail.com>
2026-05-19 10:49:03 +0545

    two sum problem solved with cpp

---
+++ twosum.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int needed = target - nums[i];
+
+        if (mp.find(needed) != mp.end()) {
+            return {mp[needed], i};
+        }
+
+        mp[nums[i]] = i;
+    }
+
+    return {};
+}
+
+int main() {
+    int n;
+    
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cout << "Enter target: ";
+    cin >> target;
+
+    vector<int> result = twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No solution found" << endl;
+    }
+
+    return 0;
+}
